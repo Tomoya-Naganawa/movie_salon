49dcebaebbc69defc7d3e11ca949c2e1b91acc16
rope <rope19181@gmail.com>
2020-04-09 22:00:38 +0900

    Add settings
    DB_CONNECTION,
    testing db,
    facebook google apis setting,
    socialite provider aliase,

--- config/services.php
+++ config/services.php
@@ -34,4 +34,16 @@ return [
         'secret' => env('SPARKPOST_SECRET'),
     ],
 
+    'facebook' => [
+        'client_id' =>  env('FACEBOOK_APP_ID'),
+        'client_secret' => env('FACEBOOK_APP_SECRET'),
+        'redirect' =>  env('FACEBOOK_CALLBACK_URL'),
+    ],
+
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_CALLBACK_URL'),
+    ],
+
 ];
