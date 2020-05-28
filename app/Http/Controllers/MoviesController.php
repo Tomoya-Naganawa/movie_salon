610b164f6299792b6eab6d103ec9a68cf6617d11
rope <rope19181@gmail.com>
2020-05-28 17:28:49 +0900

    Fix redirect exiting movie

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -14,47 +14,40 @@ class MoviesController extends Controller
 {
     public function store(Request $request, Movie $movie, Genre $genre, Actor $actor, $tmdb_movie_id)
     {
-        //まだ登録がない場合はTMDBapiを叩く
-        if (Movie::where('tmdb_id', $tmdb_movie_id)->doesntExist()) {
-            $movieService = new TmdbService;
-            $movie_array = $movieService->getMovieArray($tmdb_movie_id);
-            $credits_array = $movieService->getCreditArray($tmdb_movie_id);
-            $movie->storeMovie($movie_array);
+        $movieService = new TmdbService;
+        $movie_array = $movieService->getMovieArray($tmdb_movie_id);
+        $credits_array = $movieService->getCreditArray($tmdb_movie_id);
+        $movie->storeMovie($movie_array);
 
-            //ジャンルの登録
-            foreach ($movie_array['genres'] as $movie_genre) {
-                $movie->genres()->create([
-                'movie_id' => $movie->id,
-                'genre_id' => $movie_genre['id']
-                ]);
+        //ジャンルの登録
+        foreach ($movie_array['genres'] as $movie_genre) {
+            $movie->genres()->create([
+            'movie_id' => $movie->id,
+            'genre_id' => $movie_genre['id']
+            ]);
+        }
+
+        //出演俳優4人の登録
+        $actors = [];
+        for ($i = 0; $i <= 3; $i++) {
+            if (empty($credits_array['cast'][$i])) {
+                break;
             }
 
-            //出演俳優4人の登録
-            $actors = [];
-            for ($i = 0; $i <= 3; $i++) {
-                if (empty($credits_array['cast'][$i])) {
-                    break;
-                }
-
-                $record = $actor->firstOrCreate([
+            $record = $actor->firstOrCreate([
                 'name' => $credits_array['cast'][$i]['name'],
                 'profile_path' => $credits_array['cast'][$i]['profile_path']
-                ]);
-                array_push($actors, $record);
-            }
-
-            $actors_id = [];
-            foreach ($actors as $actor) {
-                array_push($actors_id, $actor['id']);
-            }
-            $movie->actors()->attach($actors_id);
-        
-            return redirect(url('movies/'.$movie->id));
-        //すでに登録がある場合は映画詳細画面へ遷移
-        }else{
-            $movie_id = Movie::where('tmdb_id', $tmdb_movie_id)->value('id');
-            return redirect(url('movies/'.$movie_id));
+            ]);
+            array_push($actors, $record);
         }
+
+        $actors_id = [];
+        foreach ($actors as $actor) {
+            array_push($actors_id, $actor['id']);
+        }
+        $movie->actors()->attach($actors_id);
+        
+        return redirect(url('movies/'.$movie->id));
     }
 
     public function show(Movie $movie)
--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use App\Models\Movie;
 use App\Libraries\TmdbService;
 
 class SearchController extends Controller
@@ -28,15 +29,20 @@ class SearchController extends Controller
             ]);
     }
     
-    public function show($tmdb_movie_id)
+    public function show(Movie $movie, $tmdb_movie_id)
     {
-        $TmdbService = new TmdbService;
-        $movie_array = $TmdbService->getMovieArray($tmdb_movie_id);
-        $credits_array = $TmdbService->getCreditArray($tmdb_movie_id);
+        if (Movie::where('tmdb_id', $tmdb_movie_id)->doesntExist()) {
+            $TmdbService = new TmdbService;
+            $movie_array = $TmdbService->getMovieArray($tmdb_movie_id);
+            $credits_array = $TmdbService->getCreditArray($tmdb_movie_id);
 
-        return view('tmdbs.show', [
+            return view('tmdbs.show', [
             'movie_array' => $movie_array,
             'credits_array' => $credits_array
-        ]);
+            ]);
+        }else{
+            $movie_id = Movie::where('tmdb_id', $tmdb_movie_id)->value('id');
+            return redirect(url('movies/'.$movie_id));
+        }    
     }
 }
