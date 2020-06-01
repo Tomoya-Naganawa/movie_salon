4e81cd820bd4e224468a2857deae8aa500ff3186
rope <rope19181@gmail.com>
2020-06-01 14:17:09 +0900

    Add Procfile, string length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\Schema;
 use Blade;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,16 +26,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
+        Schema::defaultStringLength(191);
+
         $url->forceScheme('https');
 
         Blade::directive('invalid', function ($name) {
             return '<?php
-              if ($errors->has('.$name.')) {
-                echo "is-invalid";
-              } else {
-                echo "";
-              }
+                if ($errors->has('.$name.')) {
+                   echo "is-invalid";
+                } else {
+                    echo "";
+                }
             ?>';
-          });
+        });
     }
 }
